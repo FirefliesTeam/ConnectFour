7146f70c844dd45082d3d2a279734b22d7bdd864
Pavel <Pivkin.Pavel.11@yandex.ru>
2015-09-16 17:39:01 +0300

    Gruntfile from second lection

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,76 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-      concat: { 
-            options:{ separator: ';'},
-            foo:{ 
-                options:{},
-                src: ['js/first.js', 'js/second.js'],
-                dest: 'js/foo.js'
+          concat: { 
+                options:{ separator: ';'},
+                foo:{ 
+                    options:{},
+                    src: ['js/first.js', 'js/second.js'],
+                    dest: 'js/foo.js'
+                },
+           },
+           
+           shell: {
+                server: {
+                    command: 'java -cp L1.2-1.0-jar-with-dependencies.jar main.Main 8080'
+                }
+           },
+           
+            fest: {
+                templates: {
+                    files: [{
+                        expand: true,
+                        cwd: 'templates',
+                        src: '*.xml',
+                        dest: 'public_html/js/tmpl'
+                    }],
+                    options: {
+                        template: function (data) {
+                            return grunt.template.process(
+                                'var <%= name %>Tmpl = <%= contents %> ;',
+                                {data: data}
+                            );
+                        }
+                    }
+                }
             },
-            bar:{}
-       },
-      //any_other_name: 'hello' /* Любое произвольное свойство */
+           
+            watch: {
+                fest: {
+                    files: ['templates/*.xml'],
+                    tasks: ['fest'],
+                    options: {
+                        interrupt: true,
+                        atBegin: true
+                    }
+                },
+                server: {
+                    files: [
+                        'public_html/js/**/*.js',
+                        'public_html/css/**/*.css'
+                    ],
+                    options: {
+                        livereload: true
+                    }
+                }
+            },
+            
+            concurrent: {
+                target: ['watch', 'shell'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }       
+           
     });
     // Загрузка плагинов, на примере "concat".
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-fest');
+    grunt.loadNpmTasks('grunt-concurrent');
+    
     // Определение задач, default должен быть всегда.
-    grunt.registerTask('default', ['concat']);
+    grunt.registerTask('default', ['concurrent']);    
     
 };
